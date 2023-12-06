371bb37335775ac93a80872ae7cab9208dcf2c14
CauevSilv <127447047+CauevSilv@users.noreply.github.com>
2023-12-06 18:59:46 -0300

    Update DDL_SGTG.sql

--- DDL_SGTG.sql
+++ DDL_SGTG.sql
@@ -2,7 +2,6 @@ CREATE SCHEMA listaGames;
 
 USE listaGames;
 
-drop table game
 
 CREATE TABLE game (
 id_game int  auto_increment PRIMARY KEY,
@@ -12,16 +11,5 @@ critica varchar(255),
 ano varchar(255),
 tamanho varchar(255),
 pirata boolean
-
-);
-
-CREATE TABLE links (
-id_link int auto_increment PRIMARY KEY,
-id_jogo int,
-link text,
-
-CONSTRAINT fk_gameid FOREIGN KEY (id_jogo)
-	REFERENCES game(id_game)
-    ON DELETE CASCADE ON UPDATE CASCADE
 
 );
